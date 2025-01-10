871d9c7e8d00ee9d74775da28a46c34d4e7c60ef
AN-UNKNOWN <anuragpindoriya@gmail.com>
2025-01-10 16:15:04 +0530

    user: improve user middleware

--- middleware/user.js
+++ middleware/user.js
@@ -1,14 +1,14 @@
 const {User} = require("../db");
 
-function userMiddleware(req, res, next) {
+async function userMiddleware(req, res, next) {
     // Implement user auth logic
     // You need to check the headers and validate the user from the user DB. Check readme for the exact headers to be expected
     const { username, password } = req.headers;
     if (!username || !password) {
         return res.status(400).json({ message: "Username and password are required in headers" });
     }
-    const userExist = User.findOne({username})
-    if(userExist){
+    const userExist = await User.findOne({username})
+    if(!userExist){
         return res.status(401).json({message:'User not found'})
     }
     if(password !== userExist.password){
