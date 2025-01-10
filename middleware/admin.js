090c0a65654fa45a1d1e2b50ae763bd7e7070645
AN-UNKNOWN <anuragpindoriya@gmail.com>
2025-01-10 12:29:10 +0530

    improve admin middleware

--- middleware/admin.js
+++ middleware/admin.js
@@ -1,9 +1,21 @@
 const {Admin}= require('../db/index') ;
 async function adminMiddleware(req,res,next){
-    const username = req.headers.username;
-    const password = req.headers.password;
-    const response = await Admin.findOne({username:username,password:password})
-    console.log(response)
-    next()
+    try{
+        const { username, password } = req.headers;
+        if (!username || !password) {
+            return res.status(400).json({ message: "Username and password are required in headers" });
+        }
+        const adminExist = await Admin.findOne({ username });
+        if (!adminExist) {
+            return res.status(401).json({ message: "Admin not found" });
+        }
+        if (password !== adminExist.password) {
+            return res.status(401).json({ message: "Invalid password" });
+        }
+        req.admin = adminExist;
+        next();
+    }catch(e){
+        res.status(500).json({message:'Internal server error'})
+    }
 }
 module.exports = adminMiddleware;
